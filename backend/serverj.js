7f0362ee573a14e2fdc03e99fd2eb61454fe2565
roshnikri03 <roshnikumari90403@gmail.com>
2026-07-27 17:33:30 +0530

    feat: set up server with Express and add mongoose dependency

---
+++ backend/serverj.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
