fa9132ed32e819e5a02a665137226df8727a3721
roshnikri03 <roshnikumari90403@gmail.com>
2026-08-01 14:45:14 +0530

    refactor: reorganize server initialization and route setup in serverj.js

--- backend/serverj.js
+++ backend/serverj.js
@@ -1,21 +1,30 @@
 import express from 'express';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import channelRoutes from './routes/channelRoutes.js';
+import videoRoutes from './routes/videoRoutes.js';
+import commentRoutes from './routes/commentRoutes.js';
 
 
 // Connect to database
 connectDB();
 
+const app = express();
 
 // Middleware
 app.use(express.json());
 
-const app = express();
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/channels', channelRoutes);
+app.use('/api/videos', videoRoutes);
+app.use('/api/comments', commentRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
